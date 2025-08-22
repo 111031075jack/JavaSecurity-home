8e6a4b2552b678c9862a49bd489ab82c25e34c46
111031075jack <111031075@live.asia.edu.tw>
2025-08-22 14:38:28 +0800

    update

--- src/aes/AESSample.java
+++ src/aes/AESSample.java
@@ -1,16 +1,34 @@
 package aes;
 
+import java.util.Arrays;
+
 import javax.crypto.spec.SecretKeySpec;
+
+import util.KeyUtil;
 
 public class AESSample {
 	
-	public static void main(String[] args) {
+	// 建立一個 AES Key (256 BITS)
+	private static final String KEY = "012345678901234567890123456789AB"; // 32個字
+	private static final String KEY2 = "AB012345678901234567890123456789"; // 32個字
+	
+	public static void main(String[] args) throws Exception {
 		// 明文
 		String originalText = "行政院宣布全民發放一萬元";
 		System.out.printf("明文: %s%n", originalText);
 		System.out.println("------------------------");
 		// 利用 AES 進行加密
-		// 1. 建立密鑰
+		// 1. 建立密鑰(AES的密鑰)
+		SecretKeySpec myKey = new SecretKeySpec(KEY.getBytes(), "AES");
+		SecretKeySpec bossKey = new SecretKeySpec(KEY2.getBytes(), "AES");
+		// 2. 將明文加密
+		byte[] encrypted = KeyUtil.encryptWithAESKey(myKey, originalText);
+		// 3. 印出密文
+		System.out.printf("密文: %s%n", Arrays.toString(encrypted));
+		System.out.println("------------------------");
+		// 4. 解密
+		String output = KeyUtil.decryptWithAESKey(myKey, encrypted); // 拿到密文去解密
+		System.out.printf("解密: %s%n", output);
 		
 		
 		
